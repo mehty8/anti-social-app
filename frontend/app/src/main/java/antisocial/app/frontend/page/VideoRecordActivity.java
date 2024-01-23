bc6a9dd7af144a001b39b073900fc560483f596d
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-01-23 15:36:11 +0100

    add empty video related activities

---
+++ frontend/app/src/main/java/antisocial/app/frontend/page/VideoRecordActivity.java
@@ -0,0 +1,13 @@
+package antisocial.app.frontend.page;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VideoRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
