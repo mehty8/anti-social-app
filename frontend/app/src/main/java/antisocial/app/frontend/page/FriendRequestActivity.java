be7ecda59918356ae0825393166ae10bdc04ee26
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-01-23 15:34:20 +0100

    add drawables for mainpage and recording video

---
+++ frontend/app/src/main/java/antisocial/app/frontend/page/FriendRequestActivity.java
@@ -0,0 +1,13 @@
+package antisocial.app.frontend.page;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FriendRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
