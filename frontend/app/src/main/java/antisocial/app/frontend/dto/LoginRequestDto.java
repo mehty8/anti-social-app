d66ddda9a09a6d506a70697cfd6eec317240cbfa
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-01-23 15:08:18 +0100

    add data transfer class for login/register request

---
+++ frontend/app/src/main/java/antisocial/app/frontend/dto/LoginRequestDto.java
@@ -0,0 +1,20 @@
+package antisocial.app.frontend.dto;
+
+public class LoginRequestDto {
+
+    private String username;
+    private String password;
+
+    public LoginRequestDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
