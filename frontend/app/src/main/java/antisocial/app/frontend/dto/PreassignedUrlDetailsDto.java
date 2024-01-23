64959f743838c14b9f333364ad3c028a7743a3ba
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-01-23 17:38:17 +0100

    add data transfer class to receive video details to watch

---
+++ frontend/app/src/main/java/antisocial/app/frontend/dto/PreassignedUrlDetailsDto.java
@@ -0,0 +1,31 @@
+package antisocial.app.frontend.dto;
+
+public class PreassignedUrlDetailsDto {
+
+    private String fileName;
+    private String httpMethod;
+    private String bucketName;
+    private int timeInMs;
+
+    public PreassignedUrlDetailsDto(String fileName, String httpMethod, String bucketName, int timeInMs) {
+        this.fileName = fileName;
+        this.httpMethod = httpMethod;
+        this.bucketName = bucketName;
+        this.timeInMs = timeInMs;
+
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+    public int getTimeInMs() {
+        return timeInMs;
+    }
+}
