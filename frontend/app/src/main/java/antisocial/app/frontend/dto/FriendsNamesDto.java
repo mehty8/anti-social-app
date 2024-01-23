af82b2c8f39a59fb0b478bf0e8fcc0dc01ff6296
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-01-23 15:37:52 +0100

    add data transfer classes to receive friends name, and videodetails to watch. Also a class for a simple response message

---
+++ frontend/app/src/main/java/antisocial/app/frontend/dto/FriendsNamesDto.java
@@ -0,0 +1,15 @@
+package antisocial.app.frontend.dto;
+
+import java.util.List;
+
+public class FriendsNamesDto {
+    private List<String> friendsNames;
+
+    public FriendsNamesDto(List<String> friendsNames) {
+        this.friendsNames = friendsNames;
+    }
+
+    public List<String> getFriendsNames() {
+        return friendsNames;
+    }
+}
