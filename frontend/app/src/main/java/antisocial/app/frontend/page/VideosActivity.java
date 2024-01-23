14bd70212bc7e5b7c9479328588d3b8b660d7499
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-01-23 17:01:53 +0100

    add methods to VideosActivity so it can load the video links

--- frontend/app/src/main/java/antisocial/app/frontend/page/VideosActivity.java
+++ frontend/app/src/main/java/antisocial/app/frontend/page/VideosActivity.java
@@ -1,13 +1,62 @@
 package antisocial.app.frontend.page;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
+import antisocial.app.frontend.R;
+import antisocial.app.frontend.dto.VideoDetailsToPlay;
+
 public class VideosActivity extends AppCompatActivity {
+    private List<VideoDetailsToPlay> videos;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        videos = (List<VideoDetailsToPlay>) intent.getSerializableExtra("videos");
+        setContentView(R.layout.activity_videos);
+        LinearLayout linearLayout = findViewById(R.id.containerLayout);
+        loadVideos(linearLayout, intent.getStringExtra("type"));
+
+    }
+
+    private void loadVideos(LinearLayout linearLayout, String type){
+        videos.forEach(video -> {
+            String videoName = video.getVideoName();
+            String toOrFrom = video.getToOrFrom();
+            String nameToDisplay = videoName + (type.equals("sent") ? "to " : "from ") + toOrFrom;
+            TextView textView = new TextView(this);
+            textView.setText(nameToDisplay);
+            textView.setBackgroundResource(R.drawable.rectangle_curvy_background);
+            textView.setPadding(16, 16, 16, 16);
+            textView.setTag(video.getPreassignedUrl());
+
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String preassignedUrl = (String) view.getTag();
+                    Intent intent = new Intent(VideosActivity.this, VideoPlayActivity.class);
+                    intent.putExtra("url", preassignedUrl);
+                    startActivity(intent);
+                }
+            });
+
+            linearLayout.addView(textView);
+
+            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) textView.getLayoutParams();
+            layoutParams.setMargins(0, 0, 0, 16);
+            textView.setLayoutParams(layoutParams);
+
+        });
     }
 }
