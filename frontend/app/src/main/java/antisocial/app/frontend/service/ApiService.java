09294f58474dc0f70cb5c90dbd06dd8cbf2cc8ec
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-01-23 15:07:33 +0100

    add request method to login and register

--- frontend/app/src/main/java/antisocial/app/frontend/service/ApiService.java
+++ frontend/app/src/main/java/antisocial/app/frontend/service/ApiService.java
@@ -1,12 +1,20 @@
 package antisocial.app.frontend.service;
 
 import antisocial.app.frontend.dto.FriendsNamesAndRequestsDTo;
+import antisocial.app.frontend.dto.JwtResponseDto;
+import antisocial.app.frontend.dto.LoginRequestDto;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ApiService {
 
-    @GET("friends")//changed to friend
+    @GET("friend")
     Call<FriendsNamesAndRequestsDTo> getFriendsNamesAndRequests(@Header("Authorization") String jwt);
+
+    @POST("user/{endpoint}")
+    Call<JwtResponseDto> loginUser(@Path("endpoint") String endpoint, @Body LoginRequestDto loginRequest);
 }
