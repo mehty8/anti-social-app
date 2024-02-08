d8f90fe4643e23c2ab9aed641ccf67af9320652c
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-02-08 19:25:39 +0100

    add username and password checking methods to registration procedure

--- frontend/app/src/main/java/antisocial/app/frontend/page/RegisterLoginActivity.java
+++ frontend/app/src/main/java/antisocial/app/frontend/page/RegisterLoginActivity.java
@@ -16,9 +16,10 @@ import java.io.IOException;
 import antisocial.app.frontend.MainActivity;
 import antisocial.app.frontend.R;
 import antisocial.app.frontend.SharedPreferencesManager;
-import antisocial.app.frontend.dto.JwtResponseDto;
-import antisocial.app.frontend.dto.RegisterLoginRequestDto;
-import antisocial.app.frontend.dto.ResponseMessageDto;
+import antisocial.app.frontend.data.PasswordCheck;
+import antisocial.app.frontend.data.dto.JwtResponseDto;
+import antisocial.app.frontend.data.dto.RegisterLoginRequestDto;
+import antisocial.app.frontend.data.dto.ResponseMessageDto;
 import antisocial.app.frontend.service.ApiClient;
 import antisocial.app.frontend.service.ApiService;
 import retrofit2.Call;
@@ -54,7 +55,22 @@ public class RegisterLoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String username = editTextUsername.getText().toString();
+                if(!checkUsername(username)){
+                    Toast.makeText(RegisterLoginActivity.this,
+                            "Username can only have letters, numbers and underscore",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
                 String password = editTextPassword.getText().toString();
+                PasswordCheck passwordCheck = checkPassword(password);
+                if(!passwordCheck.isValid()){
+                    Toast.makeText(RegisterLoginActivity.this,
+                            passwordCheck.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
                 registerUser(username, password);
             }
         });
@@ -73,18 +89,22 @@ public class RegisterLoginActivity extends AppCompatActivity {
                     startActivity(intent);
                 } else {
                     try {
-                        ResponseMessageDto responseBody = new Gson().fromJson(response.errorBody().string(), ResponseMessageDto.class);
+                        ResponseMessageDto responseBody = new Gson().fromJson(response.errorBody().string(),
+                                ResponseMessageDto.class);
                         String errorMes = responseBody.getMessage();
-                        Toast.makeText(RegisterLoginActivity.this, errorMes, Toast.LENGTH_LONG).show();
+                        Toast.makeText(RegisterLoginActivity.this,
+                                errorMes,
+                                Toast.LENGTH_LONG).show();
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
                 }
             }
-
             @Override
             public void onFailure(Call<JwtResponseDto> call, Throwable t) {
-                Toast.makeText(RegisterLoginActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                Toast.makeText(RegisterLoginActivity.this,
+                        t.getMessage(),
+                        Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -98,12 +118,16 @@ public class RegisterLoginActivity extends AppCompatActivity {
             public void onResponse(Call<ResponseMessageDto> call, Response<ResponseMessageDto> response) {
                 if(response.isSuccessful()) {
                     String responseMes = response.body().getMessage();
-                    Toast.makeText(RegisterLoginActivity.this, responseMes + ", now please login", Toast.LENGTH_LONG).show();
+                    Toast.makeText(RegisterLoginActivity.this, responseMes + ", " +
+                            "now please login",
+                            Toast.LENGTH_LONG).show();
                 } else {
                     try {
                         ResponseMessageDto responseBody = new Gson().fromJson(response.errorBody().string(), ResponseMessageDto.class);
                         String errorMes = responseBody.getMessage();
-                        Toast.makeText(RegisterLoginActivity.this, errorMes + ", please try again accordingly", Toast.LENGTH_LONG).show();
+                        Toast.makeText(RegisterLoginActivity.this,
+                                errorMes + ", please try again accordingly",
+                                Toast.LENGTH_LONG).show();
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
@@ -113,8 +137,34 @@ public class RegisterLoginActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<ResponseMessageDto> call, Throwable t) {
-                Toast.makeText(RegisterLoginActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                Toast.makeText(RegisterLoginActivity.this,
+                        t.getMessage(),
+                        Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private boolean checkUsername(String username){
+        return username.matches("\\w+");
+    }
+
+    private PasswordCheck checkPassword(String password){
+        if(!password.matches("^(?=.*[a-z])(?=.*[A-Z]).{1,}$")){
+            String message = "Password must contain at least 1 upper case and 1 lower case";
+            PasswordCheck passwordCheck = new PasswordCheck(false, message);
+            return passwordCheck;
+
+        } else if(!password.matches("^(?=.*\\d).{1,}")){
+            String message = "Password must contain at least one digit";
+            PasswordCheck passwordCheck = new PasswordCheck(false, message);
+            return passwordCheck;
+
+        } else if(!password.matches("^.{8,}$")){
+            String message = "Password must be at least 8 characters long";
+            PasswordCheck passwordCheck = new PasswordCheck(false, message);
+            return passwordCheck;
+        }
+
+        return new PasswordCheck(true, "ok");
     }
 }
