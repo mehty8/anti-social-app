caf4af83c7df9bf53dbca103137feaed48d711b2
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-05-15 20:16:33 +0200

    create new class to avoid code duplication when api response fails

---
+++ frontend/app/src/main/java/antisocial/app/frontend/service/HandleResponseFailure.java
@@ -0,0 +1,27 @@
+package antisocial.app.frontend.service;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import antisocial.app.frontend.data.dto.ResponseMessageDto;
+import retrofit2.Response;
+
+public class HandleResponseFailure {
+    public void responseError(Response<?> response, Context context, String additionalMessage){
+        try {
+            ResponseMessageDto responseBody = new Gson().fromJson(response.errorBody().string(),
+                    ResponseMessageDto.class);
+
+            String errorMessage = responseBody.getMessage() + (!additionalMessage.isEmpty() ? additionalMessage : "");
+
+            Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show();
+        } catch (IOException exception) {
+
+            throw new RuntimeException(exception);
+        }
+    }
+}
