fd43b2132eeb62aed94ea0e449f85f4bf95fa876
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-05-16 17:45:28 +0200

    refactor to minsdk level 30

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "antisocial.app.frontend"
-        minSdk = 24
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
