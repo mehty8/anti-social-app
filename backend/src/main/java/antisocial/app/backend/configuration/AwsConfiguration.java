debc1a6a2a3fb7f523b59dbaf7b292ef5bfeca83
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-06-13 18:15:38 +0200

    refactor code according to aws cloud deployment

--- backend/src/main/java/antisocial/app/backend/configuration/AwsConfiguration.java
+++ backend/src/main/java/antisocial/app/backend/configuration/AwsConfiguration.java
@@ -5,6 +5,10 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,19 +16,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AwsConfiguration {
 
-    @Value("${access.key}")
-    private String awsAccessKey;
-
-    @Value("${secret.access.key}")
-    private String awsSecretKey;
-
     @Value("${region}")
     private String region;
 
     @Bean
-    public AmazonS3 getAmazonS3Client(){
-        AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
-        return  AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                .withRegion(region).build();
+    public AmazonS3 getS3Client(){
+        return  AmazonS3ClientBuilder.standard().withRegion(region).build();
+    }
+
+    @Bean
+    public AWSSecretsManager getSecretManager(){
+        return AWSSecretsManagerClientBuilder.standard().withRegion(region).build();
+    }
+
+    @Bean
+    public AWSSimpleSystemsManagement getSystemManager(){
+        return AWSSimpleSystemsManagementClientBuilder.standard().withRegion(region).build();
     }
 }
--- backend/src/main/java/antisocial/app/backend/security/JwtUtils.java
+++ backend/src/main/java/antisocial/app/backend/security/JwtUtils.java
@@ -1,11 +1,19 @@
 package antisocial.app.backend.security;
 
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
+import com.amazonaws.services.simplesystemsmanagement.model.GetParameterRequest;
+import com.amazonaws.services.simplesystemsmanagement.model.GetParameterResult;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -14,15 +22,36 @@ import java.security.Key;
 import java.util.Date;
 
 @Component
+@Scope("singleton")
 public class JwtUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-    @Value("${antisocial.app.jwtSecret}")
+    @Value("${secret.jwt.name}")
+    private String jwtSecretName;
+
+    @Value("${parameter.jwt.expirationTime}")
+    private String jwtExpirationTimeParameter;
+
+    private AWSSecretsManager awsSecretsManager;
+
+    private AWSSimpleSystemsManagement awsSimpleSystemsManagement;
+
     private String jwtSecret;
 
-    @Value("${antisocial.app.jwtExpirationMs}")
-    private int jwtExpirationMs;
+    private int jwtExpirationTime;
+
+
+    public JwtUtils(AWSSecretsManager awsSecretsManager, AWSSimpleSystemsManagement awsSimpleSystemsManagement) {
+        this.awsSecretsManager = awsSecretsManager;
+        this.awsSimpleSystemsManagement = awsSimpleSystemsManagement;
+    }
+
+    @PostConstruct
+    private void initJwtSetters(){
+        setJwtSecret();
+        setJwtExpirationTime();
+    }
 
 
     public String generateJwtToken(Authentication authentication) {
@@ -32,7 +61,7 @@ public class JwtUtils {
         return Jwts.builder()
                 .setSubject((userPrincipal.getUsername()))
                 .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationTime))
                 .signWith(key(), SignatureAlgorithm.HS256)
                 .compact();
     }
@@ -61,5 +90,21 @@ public class JwtUtils {
         }
 
         return false;
+    }
+
+    private void setJwtSecret(){
+        if(jwtSecret == null){
+            GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest().withSecretId(jwtSecretName);
+            GetSecretValueResult getSecretValueResult = awsSecretsManager.getSecretValue(getSecretValueRequest);
+            jwtSecret = getSecretValueResult.getSecretString();
+        }
+    }
+
+    private void setJwtExpirationTime(){
+        if(jwtExpirationTime == 0){
+            GetParameterRequest request = new GetParameterRequest().withName(jwtExpirationTimeParameter).withWithDecryption(false);
+            GetParameterResult result = awsSimpleSystemsManagement.getParameter(request);
+            jwtExpirationTime = Integer.parseInt(result.getParameter().getValue());
+        }
     }
 }
