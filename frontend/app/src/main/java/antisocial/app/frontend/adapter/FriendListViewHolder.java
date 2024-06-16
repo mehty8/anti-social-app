b7f595dccb3621b4aeebce324d81b08d934888f3
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-06-16 16:42:16 +0200

    refactor to check jwt expiration before start making video

--- frontend/app/src/main/java/antisocial/app/frontend/adapter/FriendListViewHolder.java
+++ frontend/app/src/main/java/antisocial/app/frontend/adapter/FriendListViewHolder.java
@@ -1,26 +1,37 @@
 package antisocial.app.frontend.adapter;
 
+import static antisocial.app.frontend.service.CheckJwtExpiration.expired;
+
 import android.content.Context;
 import android.content.Intent;
 import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONException;
+
+import antisocial.app.frontend.MainActivity;
 import antisocial.app.frontend.R;
+import antisocial.app.frontend.SharedPreferencesManager;
 import antisocial.app.frontend.page.VideoRecordActivity;
 
 public class FriendListViewHolder extends RecyclerView.ViewHolder{
 
     private TextView textView;
     private Context context;
+    private SharedPreferencesManager sharedPreferencesManager;
 
     public FriendListViewHolder(@NonNull View itemView, Context context) {
         super(itemView);
         this.textView = itemView.findViewById(R.id.textViewFriendListItem);
         this.context = context;
+        this.sharedPreferencesManager = new SharedPreferencesManager(context.getApplicationContext());
     }
 
     public void bind(String friendName){
@@ -32,9 +43,34 @@ public class FriendListViewHolder extends RecyclerView.ViewHolder{
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context, VideoRecordActivity.class);
-                intent.putExtra("username", friendName);
-                context.startActivity(intent);
+                try {
+                    if(expired(sharedPreferencesManager.getJwt(), -60)){
+                        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                        LayoutInflater layoutInflater = LayoutInflater.from(context);
+                        View dialogView = layoutInflater.inflate(R.layout.dialog_logout, null);
+                        builder.setView(dialogView);
+                        Button buttonLogout = dialogView.findViewById(R.id.buttonLogout);
+
+                        AlertDialog dialog = builder.create();
+
+                        buttonLogout.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                sharedPreferencesManager.deleteJwt();
+                                Intent intent = new Intent(context, MainActivity.class);
+                                context.startActivity(intent);
+                                dialog.dismiss();
+                            }
+                        });
+                        dialog.show();
+                    } else {
+                        Intent intent = new Intent(context, VideoRecordActivity.class);
+                        intent.putExtra("username", friendName);
+                        context.startActivity(intent);
+                    }
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
     }
