bd4c273b747f6efd5ec44cf3b449b894e1917eb6
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-06-16 18:32:36 +0200

    refactor to switch between development and production environment

--- frontend/app/src/main/java/antisocial/app/frontend/service/api/ApiClient.java
+++ frontend/app/src/main/java/antisocial/app/frontend/service/api/ApiClient.java
@@ -2,10 +2,11 @@ package antisocial.app.frontend.service.api;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import antisocial.app.frontend.BuildConfig;
 
 public class ApiClient {
 
-    private final static String BASE_URL = "http://anti-social-app.eu-central-1.elasticbeanstalk.com/";
+    private final static String BASE_URL = BuildConfig.BASE_URL;
 
     public static ApiService getApiServiceDynamic(){
         return new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
