a1c2c35c6d739a007a9b72c982673cf01ac018e1
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2024-06-17 13:42:29 +0200

    increment version number

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "antisocial.app.frontend"
         minSdk = 30
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
